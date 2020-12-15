8d9e5a46467830c34c55f73fe3c3ddb61c01a293
Volodymyr Polishchuk <volodymyrpo@softwareplanet.uk.com>
2020-12-15 20:10:31 +0200

    DP-241 Create Hadoop connector
    - Implement File System to run list all resources endpoint

--- src/Components/HDFileSystem.php
+++ src/Components/HDFileSystem.php
@@ -4,9 +4,16 @@ namespace DreamFactory\Core\Hadoop\Components;
 
 
 use DreamFactory\Core\File\Components\RemoteFileSystem;
+use Illuminate\Support\Facades\Log;
+use org\apache\hadoop\WebHDFS;
 
 class HDFileSystem extends RemoteFileSystem
 {
+
+    /**
+     * @var WebHDFS $webHDFSClient
+     */
+    protected $webHDFSClient;
 
     /**
      * @param array $config
@@ -15,6 +22,14 @@ class HDFileSystem extends RemoteFileSystem
      */
     public function __construct($config)
     {
+        $this->webHDFSClient = new WebHDFS(
+            $config['host'],
+            $config['port'],
+            $config['user'],
+            $config['namenode_rpc_host'],
+            $config['namenode_rpc_port'],
+            config('app.debug')
+        );
     }
 
     /**
@@ -30,6 +45,8 @@ class HDFileSystem extends RemoteFileSystem
     public function listContainers($include_properties = false)
     {
         // TODO: Implement listContainers() method.
+        Log::error('listContainers - $include_properties = ' . $include_properties);
+        return [];
     }
 
     /**
@@ -37,7 +54,9 @@ class HDFileSystem extends RemoteFileSystem
      */
     public function containerExists($container)
     {
-        // TODO: Implement containerExists() method.
+        Log::error('containerExists - $container = ' . $container);
+        $response = json_decode($this->webHDFSClient->getFileStatus($container));
+        return !isset($response->RemoteException);
     }
 
     /**
@@ -46,6 +65,7 @@ class HDFileSystem extends RemoteFileSystem
     public function getContainer($container, $include_files = true, $include_folders = true, $full_tree = false)
     {
         // TODO: Implement getContainer() method.
+        Log::error('getContainer - $container = ' . $container . '; $include_files = ' . $include_files . '; $include_folders = ' . $include_folders . '; $full_tree = ' . $full_tree);
     }
 
     /**
@@ -54,6 +74,7 @@ class HDFileSystem extends RemoteFileSystem
     public function getContainerProperties($container)
     {
         // TODO: Implement getContainerProperties() method.
+        Log::error('getContainerProperties - $container = ' . $container);
     }
 
     /**
@@ -62,6 +83,7 @@ class HDFileSystem extends RemoteFileSystem
     public function createContainer($container, $check_exist = false)
     {
         // TODO: Implement createContainer() method.
+        Log::error('createContainer - $container = ' . $container . '; $check_exist = ' . $check_exist);
     }
 
     /**
@@ -70,6 +92,7 @@ class HDFileSystem extends RemoteFileSystem
     public function updateContainerProperties($container, $properties = [])
     {
         // TODO: Implement updateContainerProperties() method.
+        Log::error('updateContainerProperties - $container = ' . $container . '; $properties = ' . implode('|', $properties));
     }
 
     /**
@@ -78,6 +101,7 @@ class HDFileSystem extends RemoteFileSystem
     public function deleteContainer($container, $force = false)
     {
         // TODO: Implement deleteContainer() method.
+        Log::error('deleteContainer - $container = ' . $container . '; $force = ' . $force);
     }
 
     /**
@@ -85,7 +109,10 @@ class HDFileSystem extends RemoteFileSystem
      */
     public function blobExists($container, $name)
     {
-        // TODO: Implement blobExists() method.
+        Log::error('blobExists - $container = ' . $container . '; $name = ' . $name);
+        $path = $this->getPath($container, $name);
+        $response = json_decode($this->webHDFSClient->getFileStatus($path));
+        return isset($response->FileStatus);
     }
 
     /**
@@ -94,6 +121,7 @@ class HDFileSystem extends RemoteFileSystem
     public function putBlobData($container, $name, $data = null, $properties = [])
     {
         // TODO: Implement putBlobData() method.
+        Log::error('putBlobData - $container = ' . $container . '; $name = ' . $name . '; $data = ' . $data . '; $properties = ' . implode('|', $properties));
     }
 
     /**
@@ -102,6 +130,7 @@ class HDFileSystem extends RemoteFileSystem
     public function putBlobFromFile($container, $name, $localFileName = null, $properties = [])
     {
         // TODO: Implement putBlobFromFile() method.
+        Log::error('putBlobFromFile - $container = ' . $container . '; $name = ' . $name . '; $localFileName = ' . $localFileName . '; $properties = ' . implode('|', $properties));
     }
 
     /**
@@ -110,6 +139,7 @@ class HDFileSystem extends RemoteFileSystem
     public function copyBlob($container, $name, $src_container, $src_name, $properties = [])
     {
         // TODO: Implement copyBlob() method.
+        Log::error('copyBlob - $container = ' . $container . '; $name = ' . $name . '; $src_container = ' . $src_container . '; $src_name = ' . $src_name . '; $properties = ' . implode('|', $properties));
     }
 
     /**
@@ -117,7 +147,19 @@ class HDFileSystem extends RemoteFileSystem
      */
     public function listBlobs($container, $prefix = '', $delimiter = '')
     {
-        // TODO: Implement listBlobs() method.
+        Log::error('listBlobs - $container = ' . $container . '; $prefix = ' . $prefix . '; $delimiter = ' . $delimiter);
+        $listOfDirectories = $this->webHDFSClient->listDirectories($container, false, true);
+        foreach ($listOfDirectories as $directory) {
+            $directory->pathSuffix .= '/';
+        }
+        $listOfFiles = $this->webHDFSClient->listFiles($container, false, true);
+        $listOfBlobs = array_merge($listOfDirectories, $listOfFiles);
+        foreach ($listOfBlobs as $blob) {
+            $blob->name = $blob->pathSuffix;
+            $blob->hadoopType = $blob->type;
+        }
+
+        return json_decode(json_encode($listOfBlobs), true);
     }
 
     /**
@@ -126,6 +168,7 @@ class HDFileSystem extends RemoteFileSystem
     public function getBlobAsFile($container, $name, $localFileName = null)
     {
         // TODO: Implement getBlobAsFile() method.
+        Log::error('getBlobAsFile');
     }
 
     /**
@@ -134,6 +177,7 @@ class HDFileSystem extends RemoteFileSystem
     public function getBlobData($container, $name)
     {
         // TODO: Implement getBlobData() method.
+        Log::error('getBlobData');
     }
 
     /**
@@ -142,6 +186,7 @@ class HDFileSystem extends RemoteFileSystem
     public function getBlobProperties($container, $name)
     {
         // TODO: Implement getBlobProperties() method.
+        Log::error('getBlobProperties');
     }
 
     /**
@@ -150,6 +195,9 @@ class HDFileSystem extends RemoteFileSystem
     public function streamBlob($container, $name, $params = [])
     {
         // TODO: Implement streamBlob() method.
+        Log::error('streamBlob - $container = ' . $container . '; $name = ' . $name . '; $params = ' . implode($params));
+        $content = $this->webHDFSClient->open($this->getPath($container, $name));
+        echo $content;
     }
 
     /**
@@ -158,5 +206,21 @@ class HDFileSystem extends RemoteFileSystem
     public function deleteBlob($container, $name, $noCheck = false)
     {
         // TODO: Implement deleteBlob() method.
+        Log::error('deleteBlob');
+    }
+
+    /**
+     * @param $container
+     * @param $name
+     * @return string
+     */
+    protected function getPath($container, $name)
+    {
+        if (preg_match('/.*\/$/', $container)) {
+            $path = $container . $name;
+        } else {
+            $path = $container . '/' . $name;
+        }
+        return $path;
     }
 }
--- src/Services/HDFSService.php
+++ src/Services/HDFSService.php
@@ -1,5 +1,6 @@
 <?php namespace DreamFactory\Core\Hadoop\Services;
 
+use DreamFactory\Core\Exceptions\InternalServerErrorException;
 use DreamFactory\Core\File\Services\RemoteFileService;
 use DreamFactory\Core\Hadoop\Components\HDFileSystem;
 
@@ -8,9 +9,17 @@ class HDFSService extends RemoteFileService
 
     /**
      * {@inheritDoc}
+     * @throws InternalServerErrorException
      */
     protected function setDriver($config)
     {
+        $this->container = array_get($config, 'container');
+
+        if (empty($this->container)) {
+            throw new InternalServerErrorException('File service container not specified. Please check configuration for file service - ' .
+                $this->name);
+        }
+
         $this->driver = new HDFileSystem($config);
     }
 }
